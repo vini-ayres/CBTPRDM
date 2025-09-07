e836dd8ce07a67d2d0b9539e89edc5d55ca88933
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2025-09-07 16:47:10 -0300

    Adicionar arquivos de projeto.

---
+++ CBTPRDM_TP01/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Vinicius do Nascimento Ayres(CB3025675)
+// Matheus Leandro Terra Luciano(CB3024881)
+
+namespace CBTPRDM_TP01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ CBTPRDM_TP01/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Vinicius do Nascimento Ayres(CB3025675)
+// Matheus Leandro Terra Luciano(CB3024881)
+
+namespace CBTPRDM_TP01
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
