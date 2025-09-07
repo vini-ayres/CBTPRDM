c720e40c1401ba149a4139faeaac1392b0cfa086
vini-ayres <v.ayres@aluno.ifsp.edu.br>
2025-09-07 16:54:06 -0300

    TP01

---
+++ TP01/CBTPRDM_TP01/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CBTPRDM_TP01")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CBTPRDM_TP01.Pages")]
---
+++ TP01/CBTPRDM_TP01/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CBTPRDM_TP01
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
