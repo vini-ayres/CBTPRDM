2d2b5c60fbfc7fee692181241d3eb59ec5c60f9a
Vinícius Ayres <v.ayres@aluno.ifsp.edu.br>
2025-09-07 16:55:16 -0300

    Delete CBTPRDM_TP01 directory

--- CBTPRDM_TP01/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CBTPRDM_TP01")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CBTPRDM_TP01.Pages")]
